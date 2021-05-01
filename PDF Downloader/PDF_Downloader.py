94e5c15a44a63bb843ee8b91570a572b6063fbbd
Tirth Nagar <tirthnagar.n1@gmail.com>
2021-05-01 19:18:24 -0400

    Update PDF_Downloader.py

--- PDF Downloader/PDF_Downloader.py
+++ PDF Downloader/PDF_Downloader.py
@@ -1,11 +1,11 @@
 import requests
 
-for y in range(1,7):
-    file_url = "http://panchbhaya.weebly.com/uploads/1/3/7/0/13701351/phys11_sm_13_" + str(y) + ".pdf"
+for y in range(1,6):
+    file_url = "http://panchbhaya.weebly.com/uploads/1/3/7/0/13701351/phys11_sm_7_" + str(y) + ".pdf"
 
     r = requests.get(file_url, stream = True)
     
-    with open("Chapter_13_Lesson"+str(y)+".pdf","wb") as pdf:
+    with open("Chapter_7_Lesson"+str(y)+".pdf","wb") as pdf:
         for chunk in r.iter_content(chunk_size=1024):
     
             # writing one chunk at a time to pdf file
